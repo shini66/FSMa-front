9edb0ab511cadafd92b6a12c29255494b21c2c95
Carlos Manuel Vargas <cmv-66@hotmail.com>
2026-07-07 20:42:45 -0300

    feat: update cart example

--- src/components/example/cart.jsx
+++ src/components/example/cart.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import header from "../../helpers/title"
 
+function totalCartPrice(cart){
+    return cart.reduce((total, cart) => total + cart.precio, 0);
+}
 
 
 function Cart(){
     const [productName, setProductName] = useState('');
-    const [cart, setCart] = useState([{ type: 'Existen', nombre: 'Teclado' }, { type: 'Existen', nombre: 'Auricular' }]);
+    const [productPrice, setProductPrice] = useState('');
+    const [cart, setCart] = useState([{ type: 'Existen', nombre: 'Teclado', precio: 100 }, { type: 'Existen', nombre: 'Auricular', precio: 100 }]);
 
     const addProduct = (product) => {
         setCart((prevCart) => [...prevCart, product])
+        setProductName(''); setProductPrice('');
     }
 
     return (
@@ -16,11 +21,13 @@ function Cart(){
             <h4>{header(cart, 'Cantidad de elemento:')}</h4>
             <ul>
                 {cart.map((producto, index) => (
-                    <li key={index}>Estado: {producto.type} - Nombre: {producto.nombre}</li>
+                    <li key={index}>Estado: {producto.type} - Nombre: {producto.nombre}/Precio: {producto.precio}</li>
                 ))}
             </ul>
+            <h4>{header(totalCartPrice(cart), 'Total de Caja:')}</h4>
             <input value={productName} onChange={(e) => setProductName(e.target.value)}></input>
-            <button onClick={() => addProduct({ type: 'Nuevo', nombre: productName })}>
+            <input value={productPrice} onChange={(e) => setProductPrice(e.target.value)}></input>
+            <button onClick={() => addProduct({ type: 'Nuevo', nombre: productName, precio: Number(productPrice) })}>
                 Agregar Producto
             </button>
         </div>
